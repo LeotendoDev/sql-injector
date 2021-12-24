a1b29296f6a33fba3454fdfa0535a9f43f134db1
Leonard Steinhoff <70445510+LeotendoDev@users.noreply.github.com>
2021-12-24 09:03:31 +0000

    Add files via upload

--- injector.py
+++ injector.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 
-import socket
+import urllib.request
 import sys
 import time
 
@@ -9,19 +9,19 @@ port = sys.argv[2]
 header = sys.argv[3]
 payload_list = sys.argv[4]
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 try:
     client.connect((target, int(port)))
 except ConnectionError:
     print('[-] Cannot conenct to target!')
 
 def send_payloads():
+    print('[*] Connecting to target...')
     print('[*] Sending payloads...')
 
     stream_reader = open(payload_list, 'r')
     for payload in stream_reader:
-        client.send(payload.encode('utf8'))
+        server = urllib.request.urlopen(f'{target}:{port}/index.php?{header}=Buffer{payload}')
+
         print('[*] PAYLOAD: ' + payload, end='')
 
         time.sleep(0.02)
